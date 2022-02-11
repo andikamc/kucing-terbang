f57e1308f85497f56d34e3d82c8686baedf79618
Andika Muhammad Cahya <andkmc99@gmail.com>
2022-02-11 15:00:37 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,8 +8,7 @@ sudo apt upgrade -y --fix-missing
 update-grub
 sysctl -w net.ipv6.conf.all.disable_ipv6=1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1
-apt update && sudo apt install -y wget
-sudo apt install -y bzip2 gzip coreutils screen curl shc gcc
+sudo apt install -y wget bzip2 gzip build-essential coreutils screen curl shc gcc 
 wget https://raw.githubusercontent.com/andikamc/kucing-terbang/kucingku/setup.sh
 chmod +x setup.sh
 shc -f setup.sh -o setup.sh && rm setup.sh.*
