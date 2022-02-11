a5b2d857e172cc2efc80813d635b344bb1a54ce8
Andika Muhammad Cahya <andkmc99@gmail.com>
2022-02-11 12:16:49 +0700

    Update update-xws.sh

--- update-xws.sh
+++ update-xws.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /usr/bin
 wget -O update "https://raw.githubusercontent.com/andikamc/kucing-terbang/master/update.sh"
+chmod +x update
 wget -O port-trgo "https://raw.githubusercontent.com/andikamc/kucing-terbang/master/port-trgo.sh"
+chmod +x port-trgo
