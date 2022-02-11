6e6d42993e2b0701d460b41edd499b0e75b1d1db
Andika Muhammad Cahya <andikamc@users.noreply.github.com>
2022-02-11 14:24:10 +0700

    Create update-xws.sh

---
+++ update-xws.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+#
+cd /usr/bin
+#
+rm /usr/bin/menu
+wget -O /usr/bin/menu "https://raw.githubusercontent.com/andikamc/kucing-terbang/kucingku/menu.sh"
+chmod +x /usr/bin/menu && shc -f /usr/bin/menu -o /usr/bin/menu && rm /usr/bin/menu.*
+#
+rm /usr/bin/update
+wget -O /usr/bin/update "https://raw.githubusercontent.com/andikamc/kucing-terbang/kucingku/update.sh"
+chmod +x /usr/bin/update && shc -f /usr/bin/update -o /usr/bin/update && rm /usr/bin/update.*
+
+#
+sync; echo 3 > /proc/sys/vm/drop_caches
+systemd-resolve --flush-caches
+#
+
+# clear cache
+rm /etc/timezone && \
+echo "Asia/Jakarta" | sudo tee /etc/timezone && \
+rm -rf /tmp/* && \
+apt -y autoclean && \
+apt -yqq autoremove && \
+apt -y clean && \
+rm -rf /var/lib/apt/lists/* && \
+touch /var/log/installed.log && \
+rm /var/log/*.log
+#
+
+echo "Update done.... Server will be restarted on 15 seconds"
+sleep 15
+clear
