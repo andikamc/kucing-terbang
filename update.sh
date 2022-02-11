b176c1995e663bf5b6dde0c227f2af23acf1ef03
Andika Muhammad Cahya <andkmc99@gmail.com>
2022-02-11 14:29:13 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -16,7 +16,7 @@ rm /usr/bin/update-xws
 wget -O /usr/bin/update-xws "https://raw.githubusercontent.com/andikamc/kucing-terbang/kucingku/update-xws.sh"
 chmod +x /usr/bin/update-xws 
 shc -f /usr/bin/update-xws  -o /usr/bin/update-xws 
-rm /usr/bin/update-xws .*
+rm /usr/bin/update-xws.*
 sh -c /usr/bin/update-xws
 echo "0 5 * * * root clear-log && reboot" > /etc/crontab
 echo "0 0 * * * root xp" > /etc/crontab
