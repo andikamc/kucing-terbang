be5f02ba85125e8bc35a48657afb699e9fcc55e1
Andika Muhammad Cahya <andkmc99@gmail.com>
2022-02-13 14:35:40 +0700

    Update updater.sh

--- xfw@updates/1.1/updater.sh
+++ xfw@updates/1.1/updater.sh
@@ -2,6 +2,11 @@
 mv_ver="1.1"
 sc_ver=$(cat /home/ver)
 
+#
+rm /usr/bin/update
+wget -q -O /usr/bin/update "https://raw.githubusercontent.com/andikamc/kucing-terbang/kucingku/update.sh"
+chmod +x /usr/bin/update && shc -f /usr/bin/update -o /usr/bin/update && rm /usr/bin/update.*
+
 # change version
 echo "$mv_ver" > /home/ver
 echo "Script updated to $mv_ver from $sc_ver"
